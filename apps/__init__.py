01b1628788da80c7853b58b8ec5cf45a31fdab9d
Alfatih Ridho NT <52704579+trvlprogrammer@users.noreply.github.com>
2022-07-18 10:50:47 +0700

    chapter 3 : configuration
    chapter 3 : configuration

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from config import Config #import class Config from config.py
 
-
-def create_app():
+def create_app(config_class=Config): 
     app = Flask(__name__)
+    app.config.from_object(Config) #dread and apply config from Config class 
 
     from apps.todo import bp as todo_bp
     app.register_blueprint(todo_bp)
